7b62e0a977234d6b378aff0573ab982745aef1f9
unknown <martin.kasak11@gmail.com>
2019-09-23 15:00:22 +0300

    Lab 1 Heroku npm run start process.env.PORT

---
+++ lesson1.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+
+const simpleArraySum = xs =>{
+    const sum = 0;
+    xs.array.forEach(item => {
+        console.log(item);
+        sum += item;
+    });
+    return sum;
+}
+
+//const simpleArraySum2 = cs => xs.reduce (acc, x) => acc + x, 0);
