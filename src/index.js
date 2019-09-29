a397b24aa9d6c0dd964c9b892012d4eb5a50b740
unknown <martin.kasak11@gmail.com>
2019-09-30 01:52:02 +0300

    Ülesanne 5 (React Hello World)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
-/* const homepage = require("./homepage.js");
-const itempage = require("./itempage.js");
- */
+const node = document.getElementById("app");
 
- import homepage from "./homepage.js";
- import itempage from "./itempage.js";
-
- console.log("i am index file");
-
-window.addEventListener("load", () =>{
-  homepage.setup();
-  itempage.setup();
-});
+ReactDOM.render(
+  React.createElement(
+    "button",
+    {},
+    "I am a button, Hello world"
+  ),
+  node,
+);
