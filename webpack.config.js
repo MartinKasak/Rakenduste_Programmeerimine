8c99533e23ec0244c3d240833199fcb8ff7e97fb
unknown <martin.kasak11@gmail.com>
2019-09-30 01:32:58 +0300

    Ülesanne 4 (using bable ja webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,17 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      }
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyPlugin([
