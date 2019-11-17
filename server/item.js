d8f88327c3758e7863e77060eea99da9ca41df4b
unknown <martin.kasak11@gmail.com>
2019-11-17 23:43:21 +0200

    delete ül1

--- server/item.js
+++ server/item.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-//const DB = require("./database.js");
+//const DB = require("./database.js"); //
 const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
