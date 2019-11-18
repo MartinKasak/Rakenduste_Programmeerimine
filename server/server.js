bd76b45924cd4a70731e9acde46980385ee41d9c
unknown <martin.kasak11@gmail.com>
2019-11-18 03:14:04 +0200

    heroku

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
-require("dotenv").config();
 const itemRouter = require("./item.router.js");
 const userRouter = require("./user.router.js");
 const DB = require("./database.js");
 const Item = require("./item.model.js");
 const bodyParser= require("body-parser");
+
+if(process.env.NODE_ENV !== "production"){
+    require("dotenv").config();
+  }
 
 const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-pmrpm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 app.use(bodyParser.json());
