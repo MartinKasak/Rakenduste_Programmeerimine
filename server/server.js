d9a03f7abda9b8b7f0549279f698e4ccffac04a4
unknown <martin.kasak11@gmail.com>
2019-12-19 22:32:39 +0200

    server.js olid vead sees

--- server/server.js
+++ server/server.js
@@ -1,29 +1,26 @@
+if(process.env.NODE_ENV !== "production"){
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
-const path = require("path");
-const PORT = process.env.PORT || 3000;
-
 const itemRouter = require("./item.router.js");
-const authRouter = require("./auth.router.js");
 const userRouter = require("./user.router.js");
+const authRouter = require("./auth.router.js");
 const database = require("./database.js");
-const bodyParser= require("body-parser");
-
-if(process.env.NODE_ENV !== "production"){
-    require("dotenv").config();
-  }
-
-  /** For images and bundle.js */
-app.use("/static", express.static("dist/static"));
-
-
-/** For index.html */
-app.use("/*", express.static("dist"));
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1", itemRouter);
 app.use("/api/v1/users", userRouter);
+
+/** For images and bundle.js */
+app.use("/static", express.static("dist/static"));
+
+/** For index.html */
+app.use("/*", express.static("dist"));
 
 function listen(){
   app.listen(PORT, () => {
@@ -41,12 +38,13 @@ database.connect()
   });
 
 
+// Old code below, possibly unused
+const path = require("path");
+
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
 });
 
 app.get("/items/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
 });
-
-
