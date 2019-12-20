4b2d543cf4a29edaa270f6ae9d2745f73e518f39
unknown <martin.kasak11@gmail.com>
2019-12-20 06:16:39 +0200

    .

--- server/database.js
+++ server/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Item = require("./item.model.js");
+const DB = require("./database.js");
 
 const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-pmrpm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
